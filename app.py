e0cfe7e583fedb6d3b1b70f2da92db7c689517c5
Meluwor <mukeratov@web.de>
2026-02-10 22:31:30 +0100

    added first test route

--- app.py
+++ app.py
@@ -1,3 +1,22 @@
 from flask import Flask
+from data_manager import DataManager
+from models import db, Movie
+import os
 
 app = Flask(__name__)
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'data/movies.db')}"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)  # Link the database and the app. This is the reason you need to import db from models
+
+data_manager = DataManager() # Create an object of your DataManager class
+
+
+@app.route('/')
+def home():
+    return "Welcome to MoviWeb App!"
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5002, debug=True)
